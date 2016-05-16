1fdfcc8481e4b181a34300d533af5749446ff7b1
Greggory Rothmeier <greggroth@gmail.com>
2016-05-15 22:17:15 -0700

    Fix documentation typos

--- request.go
+++ request.go
@@ -3,14 +3,14 @@ package square
 import "time"
 
 // Request parameters for fetching a payment.  If the LocationId is ommitted
-// from the GetPaymentRequest, "me" usedd as the "location_id" in the request.
+// from the GetPaymentRequest, "me" used as the "location_id" in the request.
 type GetPaymentRequest struct {
 	LocationId string
 	PaymentId  string
 }
 
 // Request parameters for fetching a collection of payment.  If the LocationId
-// is ommitted from the GetPaymentRequest, "me" usedd as the "location_id" in
+// is ommitted from the GetPaymentRequest, "me" used as the "location_id" in
 // the request.
 type ListPaymentsRequest struct {
 	LocationId string
